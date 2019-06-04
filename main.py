6159ef3a26d33e4cfea18787cfba1fd5ebc2a8fa
0x00-pl <0x00.pl@gmail.com>
2019-06-04 08:12:05 +0100

    add code

--- main.py
+++ main.py
@@ -301,6 +301,7 @@ def nfa_test():
 
 
 class env_stack:
+    no_value = {}
     def __init__(self):
         self.stack = [{}]
 
@@ -322,11 +323,20 @@ class env_stack:
         for i in range(-1, -1-len(self.stack), -1):
             if name in self.stack[i]:
                 return self.stack[i].get(name)
-        return None
+        return env_stack.no_value
+
+    def try_add(self, name, value):
+        if self.get(name) == env_stack.no_value:
+            self.add(name, value)
+            return True
+        elif self.get(name) == value:
+            return True
+        else:
+            return False
 
 
-# def rex_match_item():
-
+def rex_make_match_item_pred(env_s: env_stack, name):
+    return lambda: lambda l,idx: (True, 1) if env_s.try_add(name, l[idx]) else (False, 0)
 
 #
 #
