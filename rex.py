3b3180848de01be3937670afb944a98528352fe9
0x00-pl <0x00.pl@gmail.com>
2019-06-29 09:44:37 +0100

    add rex_env

--- rex.py
+++ rex.py
@@ -1,10 +1,35 @@
 import typing
+import utils
 
 
-class REX:
+class REXEnv:
+    VALUE_UNBOUND = object()
+
     def __init__(self):
-        self.env_stack:[{}] = []
-        self.matching_functions:[typing.Callable[[matching_iter], bool]] = []
+        self.env_stack:[dict] = [{}]
+        self.matching_functions:[typing.Callable[[utils.MatchingIter, dict], bool]] = []
 
 
-    def
+    def push_env(self):
+        self.env_stack.append({})
+
+    def pop_env(self):
+        self.env_stack.pop(-1)
+
+    def add_to_current_env(self, key, value):
+        self.env_stack[-1][key] = value
+
+    def get_value(self, key):
+        for idx in reversed(range(len(self.env_stack))):
+            ret = self.env_stack[idx].get(key, REXEnv.VALUE_UNBOUND)
+            if ret != REXEnv.VALUE_UNBOUND:
+                return ret
+
+    def match_value(self, key, value):
+        last_value = self.get_value(key)
+        if last_value == REXEnv.VALUE_UNBOUND:
+            self.add_to_current_env(key, value)
+            return True
+        else:
+            return last_value == value
+
