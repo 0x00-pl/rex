939aed8b5039b992a55844ba5fbdaf71d31117b0
0x00-pl <0x00.pl@gmail.com>
2019-07-04 11:34:03 +0100

    rewrite

--- rex.py
+++ rex.py
@@ -6,38 +6,38 @@ import utils
 class REXEnv:
     VALUE_UNBOUND = object()
 
-    def __init__(self):
-        self.env_stack: [dict] = [{}]
-        self.matching_functions: [typing.Callable[[utils.MatchingIter, dict], bool]] = []
-
-    def push_env(self):
-        self.env_stack.append({})
-
-    def pop_env(self):
-        self.env_stack.pop(-1)
-
-    def add_to_current_env(self, key, value):
-        self.env_stack[-1][key] = value
-
-    def get_value(self, key):
-        for idx in reversed(range(len(self.env_stack))):
-            ret = self.env_stack[idx].get(key, REXEnv.VALUE_UNBOUND)
-            if ret != REXEnv.VALUE_UNBOUND:
-                return ret
-
-    def match_value(self, key, value):
-        last_value = self.get_value(key)
-        if last_value == REXEnv.VALUE_UNBOUND:
-            self.add_to_current_env(key, value)
-            return True
-        else:
-            return last_value == value
-
-    def __enter__(self):
-        self.push_env()
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.pop_env()
+    # def __init__(self):
+    #     self.env_stack: [dict] = [{}]
+    #     self.matching_functions: [typing.Callable[[utils.MatchingIter, dict], bool]] = []
+    #
+    # def push_env(self):
+    #     self.env_stack.append({})
+    #
+    # def pop_env(self):
+    #     self.env_stack.pop(-1)
+    #
+    # def add_to_current_env(self, key, value):
+    #     self.env_stack[-1][key] = value
+    #
+    # def get_value(self, key):
+    #     for idx in reversed(range(len(self.env_stack))):
+    #         ret = self.env_stack[idx].get(key, REXEnv.VALUE_UNBOUND)
+    #         if ret != REXEnv.VALUE_UNBOUND:
+    #             return ret
+    #
+    # def match_value(self, key, value):
+    #     last_value = self.get_value(key)
+    #     if last_value == REXEnv.VALUE_UNBOUND:
+    #         self.add_to_current_env(key, value)
+    #         return True
+    #     else:
+    #         return last_value == value
+    #
+    # def __enter__(self):
+    #     self.push_env()
+    #
+    # def __exit__(self, exc_type, exc_val, exc_tb):
+    #     self.pop_env()
 
 
 class REX:
@@ -82,6 +82,15 @@ class REXFunc:
                 return None
 
         return with_env
+
+    @staticmethod
+    def or_(*args):
+        def or_(it: utils.MatchingIter, env: REXEnv):
+            for func in args:
+                env.push_env()
+                func(it, env)
+
+
 
 
 def test():
